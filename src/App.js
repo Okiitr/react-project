31216dbc93d03c48517b1d303be71e51e98e8acb
Oman Kumar <100030700+Okiitr@users.noreply.github.com>
2022-06-28 21:37:37 +0530

    news app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import React from "react";
 import {
   BrowserRouter as Router,
-  Switch,
-  Route,
+  Routes,
+  Route
 } from "react-router-dom";
 
-import React, { Component } from 'react'
+import { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
 
@@ -16,17 +16,16 @@ export default class App extends Component {
       <div>
         <Router>
           <NavBar />
-          <News pageSize={6} country="in" category="sports" />
-          <Switch>
-            <Route exact path="/"><News key="general" pageSize={6} country="in" category="general" /></Route>
-            <Route exact path="/home"><News key="general" pageSize={6} country="in" category="general" /></Route>
-            <Route exact path="/business"><News key="business" pageSize={6} country="in" category="business" /></Route>
-            <Route exact path="/entertainment"><News key=" ntertainment" pageSize={6} country="in" category="entertainment" /></Route>
-            <Route exact path="/health"><News key="health" pageSize={6} country="in" category="health" /></Route>
-            <Route exact path="/science"><News key="science" pageSize={6} country="in" category="science" /></Route>
-            <Route exact path="/sports"><News key="sports" pageSize={6} country="in" category="sports" /></Route>
-            <Route exact path="/technology"><News key="technology" pageSize={6} country="in" category="technology" /></Route>
-          </Switch>
+          <Routes>
+            <Route exact path="/" element={ <News key="general" pageSize={6} country="in" category="general" />}/>
+             <Route exact path="/home" element={<News key="general" pageSize={6} country="in" category="general" />}/>
+            <Route exact path="/business" element={ <News key="business" pageSize={6} country="in" category="business" />}/>
+            <Route exact path="/entertainment" element={ <News key=" ntertainment" pageSize={6} country="in" category="entertainment" />}/>
+            <Route exact path="/health" element={ <News key="health" pageSize={6} country="in" category="health" />}/>
+            <Route exact path="/science" element={ <News key="science" pageSize={6} country="in" category="science" />}/>
+            <Route exact path="/sports" element={ <News key="sports" pageSize={6} country="in" category="sports" />}/>
+            <Route exact path="/technology" element={ <News key="technology" pageSize={6} country="in" category="technology" />}/>
+          </Routes>
         </Router>
       </div>
     )
--- src/components/News.js
+++ src/components/News.js
@@ -31,7 +31,6 @@ export class News extends Component {
         let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e4ad0235e80d46a4b09f9a6ded9e096e&page=1&pageSize=${this.props.pageSize}`;
         let data= await fetch(url);
         let parsedData= await data.json();
-        console.log(parsedData.totalResults)
         this.setState({articles:parsedData.articles,
            totalResult:parsedData.totalResults,
            loading:false
