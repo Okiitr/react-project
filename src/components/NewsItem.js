6ddf98885a744af2970ab381049883d51c3d9a69
Oman Kumar <100030700+Okiitr@users.noreply.github.com>
2022-06-30 09:07:53 +0530

    news app

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,13 +6,13 @@ export class NewsItem extends Component {
         let {title,desc,imageUrl,newsUrl,date,source}=this.props
         return (
             <div>
-                <div classNameName="card"  >
-                   <img src={imageUrl} classNameName="card-img-top" alt="..."/>
-                        <div classNameName="card-body">
-                            <h5 classNameName="card-title">{title}... <span className="badge text-bg-info">{source}</span></h5>
-                            <p classNameName="card-text">{desc}...</p>
+                <div className="card"  >
+                   <img src={imageUrl} className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">{title}... <span className="badge text-bg-info">{source}</span></h5>
+                            <p className="card-text">{desc}...</p>
                             <p className="card-text"><small className="text-muted">Date-{new Date(date).toGMTString()}</small></p>
-                            <a href={newsUrl} className="btn btn-sm btn-primary">Read More</a>
+                            <a href={newsUrl} className="btn btn-sm btn-dark">Read More</a>
                         </div>
                 </div>
             </div>
